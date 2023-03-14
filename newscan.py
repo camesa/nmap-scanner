5b864949498ffb45d84be7f95b1280d0ed8c9556
camesa <72458074+camesa@users.noreply.github.com>
2023-03-14 16:36:59 -0300

    Update newscan.py

--- newscan.py
+++ newscan.py
@@ -1,56 +1,57 @@
-import ifaddr
-import nmap
-import json
-import argparse
-import requests
+#!/usr/bin/env python3 
 
+import ifaddr # module for fetching IP addresses of adapters
+import nmap # module for network exploration and management
+import json # module for json data handling
+import argparse # module for parsing command-line options
+import requests # module for sending HTTP requests
 
-#Tomar argumento de la linea de comando y parsearlo como variable para la interfaz a escanear.
+# Argument Parser to specify the network interface
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", type=str, default="eth0", dest='interfaz')
 args = ap.parse_args()
 interfaz = args.interfaz
 
-#Identificar la subred a la que pertenece la interfaz declarada por el usuario.
+# Get all adapters and their IP addresses
 adapters = ifaddr.get_adapters()
 for adapter in adapters:
     if adapter.nice_name.startswith(interfaz):
-        print("La subred para la interfaz " + adapter.nice_name + " es:")
+        print(f"La subred para la interfaz {adapter.nice_name} es:")
         for ip in adapter.ips:
-            subnet = "   %s/%s" % (ip.ip, ip.network_prefix)
+            subnet = f"   {ip.ip}/{ip.network_prefix}"
             print(subnet)
+            break # break the loop once the subnet is found
 
-#Utilizando python-nmap para escanear la subred
+# Network scanner using nmap library
 nm = nmap.PortScanner()
-print('\nEscaneando la red: '+ subnet)
-scan = nm.scan(hosts=subnet, arguments='-sV -sS -sU --open -p 21,22,53,80,88,139,445,8000,8081 -script=banner', sudo=True)
+print(f'\nEscaneando la red: {subnet}')
+scan = nm.scan(hosts=subnet, arguments='-sV -sS -sU -p 21,22,53,80,88,139,445,8000,8081 -script=banner', sudo=True)
 output = json.dumps(scan)
 print('=========================================')
 
-#Mostrar en pantalla los resultados del scan aclarando IP, puerto, protocolo y banner
+# Print scan results
 for host in nm.all_hosts():
      print('=========================================')
-     print('Host : %s (%s)' % (host, nm[host].hostname()))
-     print('Estado : %s' % nm[host].state())
+     print(f'Host : {host} ({nm[host].hostname()})')
+     print(f'Estado : {nm[host].state()}')
      for proto in nm[host].all_protocols():
          print('----------')
-         print('Protocolo : %s' % proto)
+         print(f'Protocolo : {proto}')
          lport = nm[host][proto].keys()
          for port in lport:
              if nm[host][proto][port]['state'] == 'open':
                 portinf = nm[host][proto][port]
-                print('puerto : %s\testado : %s\tbanner: %s %s\t' % (port, nm[host][proto][port]['state'], portinf['product'], portinf['extrainfo']))
+                print(f'puerto : {port}\testado : {nm[host][proto][port]["state"]}\tbanner: {portinf["product"]} {portinf["extrainfo"]}\t')
 
-
-
-#Enviando resultados del scan a la URL "http://127.0.0.1/example/fake_url.php" mediante peticion POST
+# Send scan results to a URL
 print("enviando resultados a la URL http://127.0.0.1/example/fake_url.php . . . ")
 try:
     response = requests.post('http://127.0.0.1/example/fake_url.php', json=output)
+		print("[OK]")
 except Exception as e:
     print("[FAIL]")
 
-#Exportando resultados como archivo .json
+# Generate output file
 print("Generando fichero output.json . . . ")
 try:
     jsonFile = open("output.json", "w")
